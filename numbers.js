add7f6470a77be2c4d6fd4223df708134795a488
AdamCsordas21 <adamcsordas@hotmail.com>
2020-07-30 11:19:02 +0100

    comment out numberToString implentetion

--- numbers.js
+++ numbers.js
@@ -6,7 +6,22 @@ function strToInteger(input, base) {
   return parseInt(input, base)
 }
 
+/*
+function numberToString(input, base) {
+  return input.toString(base)
+}
+
+input === 0 <= 256
+binary numbers = input.padStart(8, 0) input.toString(base).padStart(8, 0)
+octal numbers = input.padStart(0)     `0${input.toString(base)}`
+hexadecimel = input.padStart(0x)      `0x${input.toString(base)}`
+
+return something ? something : something
+
+*/
+
 module.exports = {
   strToNumber,
-  strToInteger
+  strToInteger,
+  // numberToString
 }
