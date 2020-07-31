b9720a5fd5e14df7571cbb6eabb177afad1a38f1
AdamCsordas21 <adamcsordas@hotmail.com>
2020-07-31 19:53:15 +0100

    add object.js

---
+++ objects.js
@@ -0,0 +1,38 @@
+/*
+const test = {
+  name: 'adam',
+  age: 123,
+  getAge: () => this.age,
+  getName() {
+    return this.name
+  },
+  address: {
+    line1: 'Flat 0.5',
+    line2: 'Super long Street',
+    city: 'Adaminsky',
+    postcode: 007,
+  }
+}
+const thisIsAlsoAnObject = null
+const thisIsANumberObject = new Number(3)
+const thisIsAStringObject = new String('hello')
+
+test.name
+test.age
+test.getAge()
+test.getName()
+test.address.line1
+
+const copyOfArray = [...array]
+const copyOfObject = {...object}
+
+const myNumbers = [3, 6, 2]
+// const second = myNumbers[1]
+const [first, myFav, third] = myNumbers
+const [, second] = myNumbers
+
+// const testName = test.name
+const { first: name } = test
+
+test.age = 12
+*/
