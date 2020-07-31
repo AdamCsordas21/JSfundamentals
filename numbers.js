c41d266bca1a125bf75bff19539b5a717ddb153c
AdamCsordas21 <adamcsordas@hotmail.com>
2020-07-31 18:19:24 +0100

    remove comments and add numberToCurrency

--- numbers.js
+++ numbers.js
@@ -58,23 +58,10 @@ function numberToString(input, base) {
   }
 }
 
-
-
-
-/*
-input === 0 <= 256
-binary numbers = input.padStart(8, 0) input.toString(base).padStart(8, 0)
-octal numbers = input.padStart(0)     `0${input.toString(base)}`
-hexadecimel = input.padStart(0x)      `0x${input.toString(base)}`
-
-return something ? something : something
-
-*/
-
 function numberToCurrency(number, currency) {
   return number.toLocaleString('en-US', {
     style: 'currency',
-    currency: `${currency}`,
+    currency,
   });
 }
 
