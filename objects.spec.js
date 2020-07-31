7d7a69cceacc12467d0532ea491f9a9b894d87a0
AdamCsordas21 <adamcsordas@hotmail.com>
2020-07-31 21:22:51 +0100

    add new tests to object.spec.js

--- objects.spec.js
+++ objects.spec.js
@@ -3,6 +3,10 @@ const {
   createDeepCopy,
   listProperties,
   describeProps,
+  doubleProps,
+  listPropertyValues,
+  describePropValues,
+  sumDoubledValues
 } = require('./objects')
 
 describe('objects', () => {
@@ -38,9 +42,32 @@ describe('objects', () => {
     expect(listProperties({ a: 1, b: 2 })).toEqual(['a', 'b'])
   })
 
-  it('describes the properites of an object', () => {
-    expect(describeProps({ a: 1, b: 2, c: 3})).toEqual('This object has 3 props: a, b, c')
+  it('describes the properties of an object', () => {
+    expect(describeProps({ a: 1, b: 2, c: 3 })).toEqual('This object has 3 props: a, b, c')
   })
 
-  it('types text')
+  it('doubles each property value', () => {
+    expect(doubleProps({ a: 1, b: 2, c: 3 })).toEqual({ a: 2, b: 4, c: 6 })
+  })
+
+  // it('triples each property value', () => {
+  //   expect(doubleProps({ a: 1, b: 2, c: 3 })).toEqual({ a: 3, b: 6, c: 9 })
+  // })
+
+  it('provides list of values of any object', () => {
+    expect(listPropertyValues({ a: 1, b: 2, c: 3 })).toEqual([1, 2, 3])
+  })
+
+  it('describes the property values of an object', () => {
+    expect(describePropValues({ a: 4, b: 6, c: 0 })).toEqual('This object has 3 props, with values: 4, 6 and 0')
+  })
+
+  it('doubles each property value and sums it', () => {
+    expect(sumDoubledValues({ a: 1, b: 2 })).toEqual(6)
+    expect(sumDoubledValues({ a: 1, b: 2, c: 3 })).toEqual(12)
+  })
+
+  // it('triples each property value and sums it', () => {
+  //   expect(sumTripleProps({ a: 1, b: 2, c: 3 })).toEqual(18)
+  // })
 })
