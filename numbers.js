3a35ad6cdcacc75a81d5ef409c6b054ebb5a3fe4
AdamCsordas21 <adamcsordas@hotmail.com>
2020-07-31 15:38:44 +0100

    redo strToInteger test

--- numbers.js
+++ numbers.js
@@ -2,8 +2,39 @@ function strToNumber(input) {
   return isNaN(input) ? 0 : Number(input)
 }
 
+function getListOfValidDigits(base) {
+  if (base === 2) {
+    return '01'.split('')
+  }
+  if (base === 8) {
+    return '01234567'.split('')
+  }
+  if (base === 10) {
+    return '0123456789'.split('')
+  }
+  return '0123456789abcdef'.split('')
+}
+
+function filterBaseFormatting(input, base) {
+  if (base === 16) {
+    const lowerCased = input.toLowerCase()
+    return lowerCased.startsWith('0x') ? lowerCased.substring(2) : lowerCased
+  }
+  return input
+}
+
+function isValid(input, base) {
+  for (const char of input) {
+    if (!getListOfValidDigits(base).includes(char)) {
+      return false
+    }
+  }
+  return true
+}
+
 function strToInteger(input, base) {
-  return parseInt(input, base)
+  const filtered = filterBaseFormatting(input, base)
+  return isValid(filtered, base) ? parseInt(filtered, base) : 0
 }
 
 /*
