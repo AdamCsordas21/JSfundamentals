1b31b577041b28c5a2580e025495e001d87c3d6d
AdamCsordas21 <adamcsordas@hotmail.com>
2020-08-06 20:18:05 +0100

    add datetime.js

---
+++ datetime.js
@@ -0,0 +1,38 @@
+function dayNumberToName(number) {
+  switch (number) {
+    case 0: return 'Sunday'
+    case 1: return 'Monday'
+    case 2: return 'Tuesday'
+    case 3: return 'Wednesday'
+    case 4: return 'Thursday'
+    case 5: return 'Friday'
+    case 6: return 'Saturday'
+  }
+}
+
+function dayNumberToNameInHungarian(number) {
+  switch (number) {
+    case 0: return 'vasárnap'
+    case 1: return 'hétfő'
+    case 2: return 'kedd'
+    case 3: return 'szerda'
+    case 4: return 'csütörtök'
+    case 5: return 'péntek'
+    case 6: return 'szombat'
+  }
+}
+
+function getWeekDay(weekDayNumber, numberTranslator) {
+  return numberTranslator(weekDayNumber)
+}
+
+function getPartyIndicator(currentDay) {
+  return currentDay === 'Friday' ? '🎉' : '😞'
+}
+
+module.exports = {
+  getWeekDay,
+  dayNumberToName,
+  dayNumberToNameInHungarian,
+  getPartyIndicator,
+}
