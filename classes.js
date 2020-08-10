4bb7ab3e71eb0a3ae7f98f5572442dbc0994ee2a
AdamCsordas21 <adamcsordas@hotmail.com>
2020-08-10 19:58:52 +0100

    add class FoodShop

--- classes.js
+++ classes.js
@@ -15,6 +15,24 @@ class Shop {
   }
 }
 
+class FoodShop {
+  #alwaysInStock = [
+    { name: 'candies', price: '£9' }
+  ]
+  #items
+
+  constructor(items) {
+    this.#items = [...this.#alwaysInStock, ...items]
+    // this.#items = items
+  }
+
+  get items() {
+    return this.#items
+    // return [...this.#items, ...this.#alwaysInStock]
+  }
+}
+
 module.exports = {
   Shop,
+  FoodShop
 }
--- classes.spec.js
+++ classes.spec.js
@@ -1,22 +1,37 @@
 const {
   Shop,
+  FoodShop
 } = require('./classes')
 
 describe('class fundamentals', () => {
   describe('shopping', () => {
     it('can instantiate a shop class', () => {
-      const items = [
-        { name: 'phone', price: '£2' },
-        { name: 'laptop', price: '£3' },
-      ]
-      const testShop = new Shop(items)
-      expect(testShop.items).toEqual(items)
+      new Shop([])
+    })
+
+    it('provides current stock on request', () => {
+      const testShop = new Shop([ { name: 'coke', price: '£123'} ])
+      expect(testShop.items).toEqual([
+        { name: 'coke', price: '£123' }
+      ])
     })
 
     it('returns contact details', () => {
       const testShop = new Shop([])
       expect(testShop.contactDetails).toEqual('contact@shop.com')
     })
+  })
 
+  describe('food shop', () => {
+    it('can instantiate a food shop class', () => {
+      new FoodShop([])
+    })
+
+    it('always has candies in stock', () => {
+      const testShop = new FoodShop([])
+      expect(testShop.items).toEqual([
+        { name: 'candies', price: '£9' }
+      ])
+    })
   })
 })
