02f8739cff9da4a92a4429d1317c106939b49f3d
AdamCsordas21 <adamcsordas@hotmail.com>
2020-10-01 20:51:32 +0100

    refactor testing spec with it.each()()

--- src/testing.spec.ts
+++ src/testing.spec.ts
@@ -1,21 +1,12 @@
 import { howFast } from './testing'
 
 describe('how fast', () => {
-  it("tells it's ultra fast for speeds 200 and up", () => {
-    expect(howFast(200)).toEqual('ultra fast')
-  })
-
-  it("tells it's super fast for speeds between 100 and 199 inclusive", () => {
-    expect(howFast(150)).toEqual('super fast')
-  })
-
-  it("tells it's pretty fast for speeds between 50 and 99 inclusive", () => {
-    expect(howFast(50)).toEqual('pretty fast')
-  })
-
-  it("tells it's not too fast for speeds below 50", () => {
-    expect(howFast(25)).toEqual('not too fast')
-  })
+  it.each<[string, number]>([
+    ['ultra fast', 200],
+    ['super fast', 150],
+    ['pretty fast', 50],
+    ['not too fast', 25],
+  ])("tells it's %s for speed %d", (expected, speed) => expect(howFast(speed)).toEqual(expected))
 
   it("throws an error when speed is not a number", () => {
     // @ts-expect-error
