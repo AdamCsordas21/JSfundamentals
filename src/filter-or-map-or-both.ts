eba4c8863f1583ba94a1e4f16634c96b97bbcfd0
AdamCsordas21 <adamcsordas@hotmail.com>
2020-10-03 00:43:27 +0100

    add interface Colour to isShadeOfGreen & isShadeOfBlue

--- src/filter-or-map-or-both.ts
+++ src/filter-or-map-or-both.ts
@@ -6,18 +6,16 @@ export const isRed = ({ colour }: Colour): boolean => colour === 'red'
 
 export const isBlue = ({ colour }: Colour): boolean => colour === 'blue'
 
-export const makeCircle = ({ id, colour, sideLength }:
-  { id: string, colour: string, sideLength: number })
-  : { id: string, colour: string, shape: string, radius: number } => ({
+export const makeCircle = ({ id, colour, sideLength }) => ({
     id,
     colour,
     shape: 'circle',
     radius: sideLength / 11 * 2
   })
 
-export const isShadeOfGreen = ({ colour }: { colour: string }): boolean => colour.includes('green')
+export const isShadeOfGreen = ({ colour }: Colour): boolean => colour.includes('green')
 
-export const isShadeOfBlue = ({ colour }: { colour: string }): boolean => colour.includes('blue')
+export const isShadeOfBlue = ({ colour }: Colour): boolean => colour.includes('blue')
 
 export const doubleDigitId = ({ id, sideLength, colour }: 
   { id: string, sideLength: number, colour: string }) 
